7bf9841bf366330c9eea61b57da5e80fee7059e8
minesp3164 <lhd000721@gmail.com>
2025-04-04 05:39:21 +0900

    fix: "댓글 n개 더 보기" 버튼의 숫자가 의미하는 숫자와 다르게 표시되던 문제 수정

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1378,7 +1378,7 @@ export default function Community() {
                           </>
                         ) : (
                           <>
-                            <span>댓글 {post.comments?.filter(comment => !comment.isdeleted).length || 0}개 더 보기</span>
+                            <span>댓글 {post.comments?.filter(comment => !comment.isdeleted).length-2 || 0}개 더 보기</span>
                             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
                             </svg>
