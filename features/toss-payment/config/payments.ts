f8d13ef9128093b262b6adfaaa38e76298fd3e1e
galaxy4276 <deveungi@gmail.com>
2025-04-08 15:50:07 +0900

    fix: 빌드 오류 수정

--- features/toss-payment/config/payments.ts
+++ features/toss-payment/config/payments.ts
@@ -1,3 +1,5 @@
+'use client';
+
 export const TOSS_PAYMENTS_CONFIG = {
   clientKey: process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY || "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm",
   successUrl: `${window.location.origin}/payment/purchase/success`,
