1c85faa1eae1620fa7e54ff032bb5a23613e0500
galaxy4276 <deveungi@gmail.com>
2025-04-10 11:23:17 +0900

    fix: config 뜯어고치기 (바벨안써도됨. 프로젝트설정 잘못되어있음. )

--- next.config.js
+++ next.config.js
@@ -25,10 +25,6 @@ const nextConfig = {
   },
   // Vercel 배포에서 정적 생성 오류를 해결하기 위한 설정
   output: 'standalone',
-  experimental: {
-    // 서버 컴포넌트에서 쿠키 사용 오류 해결
-    serverActionsBodySizeLimit: '2mb',
-  },
   // 타입 체크 오류로 인한 빌드 실패 방지
   typescript: {
     // !! WARN !!
