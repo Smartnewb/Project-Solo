503950b7984ca400eb8dcb357d27b76a5092d759
Minter <adf5858@naver.com>
2025-08-29 02:29:25 +0900

    Style: 불필요한 주석 삭제 및 저장 버튼 호버 색상 변경

--- app/admin/sms/components/TemplateModal.tsx
+++ app/admin/sms/components/TemplateModal.tsx
@@ -100,10 +100,7 @@ export function TemplateModal({ isOpen, onClose, onSave }: TemplateModalProps) {
                                 </button>
                             </div>
 
-                            {/* MARK: - 모달 바디 
-                            TODO:
-                            - 자식 요소들끼리 간격 설정
-                            - 입력필드 플레이스홀더값 변경*/}
+                            {/* MARK: - 모달 바디 */}
                             <div className="px-4 sm:px-6 py-5 space-y-5">
                                     {/* 템플릿 제목 입력 */}
                                     <div>
@@ -197,9 +194,7 @@ export function TemplateModal({ isOpen, onClose, onSave }: TemplateModalProps) {
 
                                     </div>   
                                 </div>
-                            {/* MARK: - 모달 푸터 구현 
-                            TODO
-                            - 저장 아이콘 등록:*/}
+                            {/* MARK: - 모달 푸터 구현 */}
                             <div className="flex flex-col-reverse rounded-lg sm:flex-row gap-2 sm:gap-3 justify-end px-4 sm:px-6 py-3 sm:py-4 border-t bg-gray-50">
                                 {/* 취소 버튼 */}
                                 <button
@@ -211,7 +206,7 @@ export function TemplateModal({ isOpen, onClose, onSave }: TemplateModalProps) {
                                 {/* 저장 버튼 */}
                                 <button 
                                     onClick={handleSave}
-                                    className="w-full sm:w-auto px-4 py-2 text-sm font-medium bg-purple-900 text-white rounded-md hover:bg-purple-700 disabled:cursor-not-allowed transition-colors flex items-center gap-2"
+                                    className="w-full sm:w-auto px-4 py-2 text-sm font-medium bg-[#885AEB] text-white rounded-md hover:bg-purple-700 disabled:cursor-not-allowed transition-colors flex items-center gap-2"
                                     >
                                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="17" viewBox="0 0 14 17" fill="none">
                                             <path d="M2 1.5C0.896875 1.5 0 2.39688 0 3.5V13.5C0 14.6031 0.896875 15.5 2 15.5H12C13.1031 15.5 14 14.6031 14 13.5V5.91563C14 5.38438 13.7906 4.875 13.4156 4.5L11 2.08438C10.625 1.70938 10.1156 1.5 9.58438 1.5H2ZM2 4.5C2 3.94688 2.44688 3.5 3 3.5H9C9.55313 3.5 10 3.94688 10 4.5V6.5C10 7.05312 9.55313 7.5 9 7.5H3C2.44688 7.5 2 7.05312 2 6.5V4.5ZM7 9.5C7.53043 9.5 8.03914 9.71071 8.41421 10.0858C8.78929 10.4609 9 10.9696 9 11.5C9 12.0304 8.78929 12.5391 8.41421 12.9142C8.03914 13.2893 7.53043 13.5 7 13.5C6.46957 13.5 5.96086 13.2893 5.58579 12.9142C5.21071 12.5391 5 12.0304 5 11.5C5 10.9696 5.21071 10.4609 5.58579 10.0858C5.96086 9.71071 6.46957 9.5 7 9.5Z" fill="white"/>
