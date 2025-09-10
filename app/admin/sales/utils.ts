199d8cc4c72cc31f2c59d27ceb4c97e8298681cd
Minter <adf5858@naver.com>
2025-09-11 01:35:06 +0900

    feat: 통화 포매터 함수 등록

---
+++ app/admin/sales/utils.ts
@@ -0,0 +1,20 @@
+
+// MARK: - 날짜 포매터(Date -> String)
+export const formateDateToString = (date: Date): string => {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2,'0');
+        const day = String(date.getDate()).padStart(2,'0');
+        return `${year}-${month}-${day}`;
+};
+
+
+
+
+export const formatCurrency = (amount: number): string => {
+        return new Intl.NumberFormat('ko-KR', {
+                style: 'currency',
+                currency: 'KRW',
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+        }).format(amount);
+        };
