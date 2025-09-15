90604fb41954ef62bc03c1d259c76988243a6ae4
Minter <adf5858@naver.com>
2025-09-15 21:19:28 +0900

    fix: cluster 필드 삭제

--- app/services/sales.ts
+++ app/services/sales.ts
@@ -42,13 +42,11 @@ const SALES_ENDPOINT = {
 export interface GetSales {
     paymentType: paymentType;
     byRegion: boolean;
-    useCluster?: boolean;
 }
 
 export interface GetAnalysis {
     paymentType: paymentType;
     startDate: string;
-    useCluster?: boolean;
     endDate: string;
 }
 
